67eb03538c0310ad70c552cfa30c697b2cf9e9bc
Claire Walters <clairewalters@gatech.edu>
2020-03-05 13:13:30 -0500

    fix time and solved criteria

--- cartpole.py
+++ cartpole.py
@@ -47,10 +47,10 @@ def run_cartpole():
             if done:
                 scores.append(step)
                 mean_score = round(np.mean(scores), 1)
-                runtimes.append(start_time - time.time())
+                runtimes.append(time.time() - start_time)
                 print(f'Epoch: {epoch + 1}/1000 | Score: {step} | Avg Score: {mean_score} | Explore rate: {round(dqn_agent.epsilon, 4)}')
 
-                if mean_score > WINNING_SCORE and len(scores) > NUM_SCORES:
+                if mean_score >= WINNING_SCORE and len(scores) >= NUM_SCORES:
                     print(f'Solved in {epoch} epochs with a mean score of {mean_score}')
                     print(f'Runtime: {np.sum(runtimes)} | Avg runtime: {np.mean(runtimes)}')
                     exit()
